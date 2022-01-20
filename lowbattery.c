a87be415bf053ad3aaff7f0d85b6d46b20631fa9
vedpatil611 <vedpatil611@gmail.com>
2022-01-20 18:41:23 +0530

    Changed notify init and uninit scope

--- lowbattery.c
+++ lowbattery.c
@@ -29,18 +29,17 @@ int main()
     int cap;
     int flag = 0;
 
-	char file[256];
-	strcat(strcpy(file, getenv("HOME")), "/.lowbattery_lock");
+    char file[256];
+    strcat(strcpy(file, getenv("HOME")), "/.lowbattery_lock");
     int pid_file = open(file, O_CREAT | O_RDWR, 0666);
     int rc = flock(pid_file, LOCK_EX | LOCK_NB);
     if(rc) {
-        if(EWOULDBLOCK == errno)                        // another instance is running
+        if(EWOULDBLOCK == errno)				// another instance is already running
         {
             return 1;
         }
     }
 
-    notify_init("Low battery notification");
 
     for (;;sleep(1)) 
     {
@@ -51,7 +50,7 @@ int main()
         cp = readfile("/sys/class/power_supply/BAT1", "status");
         if(!strncmp(cp, "Discharging", 11))
         {
-            if(cap <= LOW_BATTERY_WARNING_THRESHOLD && (flag & 1) == 0)
+            if(cap <= LOW_BATTERY_WARNING_THRESHOLD && (flag & 1) == 0 && notify_init("Low battery notification"))
             {
                 flag |= 1;
     
@@ -59,7 +58,8 @@ int main()
                 notify_notification_set_urgency(notification, NOTIFY_URGENCY_CRITICAL);
                 notify_notification_show(notification, NULL);
 
-                g_object_unref(G_OBJECT(notification));
+				g_object_unref(notification);
+				notify_uninit();
             }
         } 
         else if(!strncmp(cp, "Charging", 8))
@@ -71,8 +71,6 @@ int main()
         }
         free(cp);
     }
-
-    notify_uninit();
 
     return 0;
 }
